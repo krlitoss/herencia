6edceda1bbfdf8b4f2bae8972b24ca561f2d9fd5
Alumno <Alumno@Maq09>
2018-05-12 11:38:56 -0500

    Clase sobre arreglos

--- Arreglos/src/arreglos/ArreglosBidimensional.java
+++ Arreglos/src/arreglos/ArreglosBidimensional.java
@@ -9,21 +9,10 @@ package arreglos;
  *
  * @author Alumno
  */
-public class Arreglos {
-
-    /**
-     * @param args the command line arguments
-     */
+public class ArreglosBidimensional {
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        // asi se declara un arreglo modo #1
-//        int arregloEntero [] = {1,2,3,4,5,6,7,8,9,10};
-       
-       // asi se declara un arreglo modo #2
-//        int [] arregloEntero  = {1,2,3,4,5,6,7,8,9,10};
-        
-        // asi se declara un arreglo modo #3
+                
         int [] [] arregloEntero = new int [10] [10];
         int n = 1;
         
@@ -32,6 +21,7 @@ public class Arreglos {
             for (int j=0; j<10; j++){
             arregloEntero [i] [j] = n;
             n++;
+            
             }
         }
         
@@ -42,10 +32,8 @@ public class Arreglos {
                 System.out.print("\t" + arregloEntero [i] [j]);
             }
             System.out.println("");
+            System.out.println("");
         }
-        
-
-        //System.out.println("Termino el ciclo for");
     }
     
 }
